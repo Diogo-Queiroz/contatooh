6a6d8479d21f342e94b1bb9526056129e04ef9b6
Diogo-Queiroz <F3l!z2804>
2015-10-17 18:42:13 -0300

    Instalação do Bower para dependencias front-end e instalação do AngularJS

--- config/express.js
+++ config/express.js
@@ -3,15 +3,20 @@
  */
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function () {
     var app = express();
 
-    // Variáveis de Ambiente
+    // Variï¿½veis de Ambiente
     app.set('port', 3000);
 
     // Middleware
     app.use(express.static('./public'));
+    app.use(bodyParser,urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(require('method-override')());
+
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
 
